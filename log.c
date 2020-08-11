066aa191e3947db279b05df0f24779dd9bdb4f0f
DrydenLinden <Drydenlinden@hotmail.com>
2020-08-11 14:28:03 -0700

    Update log.c

--- log.c
+++ log.c
@@ -17,26 +17,30 @@ double main()
 {
     int i, K = 10;
     float M;
-    double res = 1.0, add, shift; 
-    double table[10] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    
+    int r = 1, add, shift; 
+    int res = DoubleToFixed(r); 
+    int table[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     printf("Please enter a number between .5 and 1: ");
     scanf("%f", &M);
+    int m_fixed = DoubleToFixed(M);
+    
     // Flood table with information required     
     for(i=0; i <= (K-1); i++){
-        table[i] = log(1+pow(2, -i));
+        table[i] = DoubleToFixed(log(1+pow(2, -i)));
     }
     /* Loop to calculate the value of Exponential */
     for(i=0;i<=(K-1);i++)
     {
-        add = MUL(M, DoubleToFixed((1+pow(2, -i))));
+        add = MUL(m_fixed, DoubleToFixed((1+pow(2, -i))));
         shift = res - table[i];
-        if(add <= 1){
-            M = add;
+        if(add <= DoubleToFixed(1);){
+            m_fixed = add;
             res = shift;
         }
     }
      
-    printf("\nThe log Value is = %.4f\n", res);
-    return res;
+    printf("\nThe log Value is = %.4f\n", FixedToDouble(res));
+    return FixedToDouble(res);
 }
 
