1a38ca6cb6afc52bb11a52d00b528ab9eac613fc
DrydenLinden <Drydenlinden@hotmail.com>
2020-08-12 14:44:11 -0700

    Update log.c
    working

--- log.c
+++ log.c
@@ -5,44 +5,43 @@ const int scale = 16;
 #include <math.h>
 #include <stdint.h>
 
-#define DoubleToFixed(x) (x * (double)(1<<scale)) 
+#define DoubleToFixed(x) (x * (double)(1<<scale))
 #define FixedToDouble(x) ((double)x / (double)(1<<scale))
 
 
 #define MUL(x,y) ((((x)>>4)*((y)>>4))>>8)
 #define DIV(x,y) (((x)<<8)/(y)<<8)
 
- 
+
 double main()
 {
     int i, K = 10;
     float M;
-    
-    int r = 1, add, shift; 
-    int res = DoubleToFixed(r); 
+
+    int r = 0, add, shift;
+    int res = DoubleToFixed(r);
     int table[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     printf("Please enter a number between .5 and 1: ");
     scanf("%f", &M);
     int m_fixed = DoubleToFixed(M);
-    
-        
+
+
     for(i=0; i <= (K-1); i++){
         table[i] = DoubleToFixed(log(1+pow(2, -i)));
-         printf(" table location %d, value:  %d\n",i,table[i] );
+         printf(" table location %d, value:  %d \n",i,table[i] );
     }
-   
+
     for(i=0;i<=(K-1);i++)
     {
         int temp = DoubleToFixed((1.0+pow(2, -i)));
         add = MUL(m_fixed, temp);
-        shift = res - table[i];
+        shift =  - table[i];
         if(add <= DoubleToFixed(1)){
             m_fixed = add;
-            res = shift;
+            res  = shift;
         }
     }
-     
+
     printf("\nThe log Value is = %.4f\n", FixedToDouble(res));
     return FixedToDouble(res);
 }
-
