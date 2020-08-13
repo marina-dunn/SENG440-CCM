d4525a320be1274cbe277e628682b010064cef62
DrydenLinden <Drydenlinden@hotmail.com>
2020-08-13 13:15:22 -0700

    Update log.c

--- log.c
+++ log.c
@@ -41,9 +41,19 @@ double main()
             m_fixed = add;
             res = shift;
         */
+        m_fixed = ( add<=x) ? add: m_fixed;
+        res = ( add<=x) ? shift: res;
+        /*
+        asm ("cmp %2, %3 ; movle %0, %2; movle %1, %4;" 
+             : "=r"(m_fixed), "=r"(res) 
+             : "r"(add), "r"(x) , "r"(shift));  
+       */
+       /*
         asm ("cmp %0, %1" :: "r"(add), "r"(x));    
         asm ("movle %0, %1" : "=r"(m_fixed): "r"(add));
         asm ("movle %0, %1": "=r"(res): "r"(shift));
+       */
+
 /*
   asm ("cmp %2 %3 ;
   movle %3 %0;
