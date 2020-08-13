6fc92f6c1364707e92ac276f717a144609de8bf8
DrydenLinden <Drydenlinden@hotmail.com>
2020-08-13 13:48:37 -0700

    Update log.c
    not poorly written

--- log.c
+++ log.c
@@ -11,26 +11,20 @@ const int scale = 16;
 
 #define MUL(x,y) ((((x)>>4)*((y)>>4))>>8)
 #define DIV(x,y) (((x)<<8)/(y)<<8)
+const int K = 10; //precision
 
 
-double main()
-{
-    int i, K = 10;
+double Log(int table[10]){
+    int i;
     float M;
-
-    int r = 0, add, shift;
-    int res = DoubleToFixed(r);
-    int table[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     printf("Please enter a number between .5 and 1: ");
     scanf("%f", &M);
     int m_fixed = DoubleToFixed(M);
-    int x = DoubleToFixed(1);
+    int x = DoubleToFixed(1);    
+    int r = 0, add, shift;
+    int res = DoubleToFixed(r);
 
-    for(i=0; i <= (K-1); i++){
-        table[i] = DoubleToFixed(log(1+pow(2, -i)));
-         printf(" table location %d, value:  %d \n",i,table[i] );
-    }
-
+    
     for(i=0;i<=(K-1);i++)
     {
         int temp = DoubleToFixed((1.0+pow(2, -i)));
@@ -42,30 +36,30 @@ double main()
             res = shift;
         */
         add <= x ? m_fixed = add, res = shift : ;
+    
+    return FixedToDouble(res);
+}
+
+double main()
+{
+
+
+    int i;
+    int table[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+
+    for(i=0; i <= (K-1); i++){
+        table[i] = DoubleToFixed(log(1+pow(2, -i)));
+         printf(" table location %d, value:  %d \n",i,table[i] );
+    }
+    Log(table);
         /*
         asm ("cmp %2, %3 ; movle %0, %2; movle %1, %4;" 
              : "=r"(m_fixed), "=r"(res) 
              : "r"(add), "r"(x) , "r"(shift));  
-       */
-       /*
-        asm ("cmp %0, %1" :: "r"(add), "r"(x));    
-        asm ("movle %0, %1" : "=r"(m_fixed): "r"(add));
-        asm ("movle %0, %1": "=r"(res): "r"(shift));
-       */
-
-/*
-  asm ("cmp %2 %3 ;
-  movle %3 %0;
-  movle %4 %1;
-  "  : "=r"(m_fixed), "=r"(res) : "r"(add), "r"(x) , "r"(shift));     
-        */
-        
-            
-           
-        
-        
+       */   
     }
 
     printf("\nThe log Value is = %.4f\n", FixedToDouble(res));
-    return FixedToDouble(res);
+    return 0;
 }
