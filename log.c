762498fbdf9b27263c784c4582be40295114e2d3
DrydenLinden <Drydenlinden@hotmail.com>
2020-08-13 13:56:51 -0700

    Update log.c

--- log.c
+++ log.c
@@ -40,7 +40,7 @@ double Log(int table[10]){
     return FixedToDouble(res);
 }
 
-double main()
+int main()
 {
 
 
@@ -49,7 +49,7 @@ double main()
 
 
     for(i=0; i <= (K-1); i++){
-        table[i] = DoubleToFixed(log(1+pow(2, -i)));
+         table[i] = DoubleToFixed(log(1+pow(2, -i)));
          printf(" table location %d, value:  %d \n",i,table[i] );
     }
     
