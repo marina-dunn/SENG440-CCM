2c2003538e03cede05208a604ed728cd5807078b
DrydenLinden <Drydenlinden@hotmail.com>
2020-08-13 14:04:19 -0700

    Update exponentialfn.c

--- exponentialfn.c
+++ exponentialfn.c
@@ -3,46 +3,45 @@
 #include <stdint.h>
 
 const int scale = 16;
-
+const int K =10;
 #define DoubleToFixed(x) (x * (double)(1<<scale)) 
 #define FixedToDouble(x) ((double)x / (double)(1<<scale))
 
 #define MUL(x,y) ((((x)>>4)*((y)>>4))>>8)
 #define DIV(x,y) (((x)<<8)/(y)<<8)
 
-double main()
-{
-    int i, K = 10;
+
+double exponential(int table[10]){
+    
     float M;
 
-    int res = DoubleToFixed(1), add, shift; 
-    int table[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
+    int res = DoubleToFixed(1), add, shift,i; 
     printf("Please enter a number between 0 and 1: ");
     scanf("%f", &M);
     int m_fixed = DoubleToFixed(M);
     int x = DoubleToFixed(0.0);
-
-    // Flood table with information required     
-    for(i=0; i <= (K-1); i++){
-        table[i] = DoubleToFixed(log(1+pow(2, -i)));
-    }
-    /* Loop to calculate the value of Exponential */
     for(i=0;i<=(K-1);i++)
     {
         int temp = DoubleToFixed((1+pow(2, -i)));
         add = m_fixed - table[i];
         shift = MUL(res, temp);
         add >= x ? m_fixed = add, res = shift : NULL;
-        /* if(add >= DoubleToFixed(0)){
-            m_fixed = add;
-            res = shift;
-        } */ 
-        // printf("BEFORE= res: %d -- m: %d -- add: %d -- shift: %d\n", res, m_fixed, add, shift);
-        // asm ("cmp %3, %2 ; movge %2, %0; movge %4, %1;" : "=r"(m_fixed), "=r"(res) : "r"(add), "r"(x) , "r"(shift));
-        // printf("AFTER= res: %d -- m: %d -- add: %d -- shift: %d\n", res, m_fixed, add, shift);
     }
+    return FixedToDouble(res)
+}
+int main()
+{
+    int i;
+
+    int table[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    // Flood table with information required     
+    for(i=0; i <= (K-1); i++){
+        table[i] = DoubleToFixed(log(1+pow(2, -i)));
+    }
+    /* Loop to calculate the value of Exponential */
+
      
-    printf("\nThe Exponential Value is = %.4f\n", FixedToDouble(res));
-    return FixedToDouble(res);
+    printf("\nThe Exponential Value is = %.4f\n", exponential(table));
+    return 0;
 }
