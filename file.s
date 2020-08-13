25d90e828b82b45f8104585d0353015040560fd0
marina-dunn <mdunn@ugls11.ece.uvic.ca>
2020-08-13 14:15:17 -0700

    Add new asm files

--- file.s
+++ file.s
@@ -6,335 +6,334 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 2
 	.eabi_attribute 18, 4
 	.file	"exponentialfn.c"
-	.global	scale
-	.section	.rodata
-	.align	2
-	.type	scale, %object
-	.size	scale, 4
-scale:
-	.word	16
-	.align	2
-	.type	C.1.2188, %object
-	.size	C.1.2188, 40
-C.1.2188:
-	.space	40
-	.global	__aeabi_i2d
-	.global	__aeabi_d2iz
-	.align	2
-.LC0:
-	.ascii	"Please enter a number between 0 and 1: \000"
-	.align	2
-.LC1:
-	.ascii	"%f\000"
 	.global	__aeabi_f2d
 	.global	__aeabi_dmul
-	.global	__aeabi_dadd
-	.global	__aeabi_dcmpge
-	.global	__aeabi_ddiv
-	.align	2
-.LC2:
-	.ascii	"\012The Exponential Value is = %.4f\012\000"
+	.global	__aeabi_d2iz
+	.global	__aeabi_i2d
 	.text
+	.align	2
+	.global	exponential
+	.type	exponential, %function
+exponential:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 0, uses_anonymous_args = 0
+	stmfd	sp!, {r4, lr}
+	mov	r4, r0
+	sub	sp, sp, #8
+	ldr	r0, .L15
+	bl	printf
+	add	r1, sp, #4
+	ldr	r0, .L15+4
+	bl	scanf
+	ldr	r0, [sp, #4]	@ float
+	bl	__aeabi_f2d
+	mov	r3, #1073741824
+	mov	r2, #0
+	add	r3, r3, #15728640
+	bl	__aeabi_dmul
+	bl	__aeabi_d2iz
+	ldr	r3, [r4, #0]
+	rsb	r3, r3, r0
+	cmp	r3, #0
+	ldr	r2, [r4, #4]
+	movge	r0, r3
+	movge	r1, #131072
+	movlt	r1, #65536
+	subs	ip, r0, r2
+	movpl	r2, r1, asr #4
+	movpl	r3, r2, asl #13
+	subpl	r3, r3, r2, asl #11
+	movpl	r1, r3, asr #8
+	ldr	r3, [r4, #8]
+	movpl	r0, ip
+	subs	ip, r0, r3
+	bmi	.L5
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #10
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L5:
+	ldr	r3, [r4, #12]
+	subs	ip, r0, r3
+	bmi	.L6
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #9
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L6:
+	ldr	r3, [r4, #16]
+	subs	ip, r0, r3
+	bmi	.L7
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #8
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L7:
+	ldr	r3, [r4, #20]
+	subs	ip, r0, r3
+	bmi	.L8
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #7
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L8:
+	ldr	r3, [r4, #24]
+	subs	ip, r0, r3
+	bmi	.L9
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #6
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L9:
+	ldr	r3, [r4, #28]
+	subs	ip, r0, r3
+	bmi	.L10
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #5
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L10:
+	ldr	r3, [r4, #32]
+	subs	ip, r0, r3
+	bmi	.L11
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #4
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L11:
+	ldr	r3, [r4, #36]
+	rsb	r3, r3, r0
+	cmp	r3, #0
+	movge	r3, r1, asr #4
+	movge	r2, r3, asl #12
+	movge	r3, r3, asl #3
+	addge	r3, r3, r2
+	movge	r1, r3, asr #8
+	mov	r0, r1
+	bl	__aeabi_i2d
+	mov	r3, #1052770304
+	mov	r2, #0
+	add	r3, r3, #3145728
+	bl	__aeabi_dmul
+	add	sp, sp, #8
+	ldmfd	sp!, {r4, lr}
+	bx	lr
+.L16:
+	.align	2
+.L15:
+	.word	.LC0
+	.word	.LC1
+	.size	exponential, .-exponential
 	.align	2
 	.global	main
 	.type	main, %function
 main:
 	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 72
-	@ frame_needed = 1, uses_anonymous_args = 0
-	stmfd	sp!, {r4, r5, r6, r7, r8, fp, lr}
-	add	fp, sp, #24
-	sub	sp, sp, #76
-	mov	r3, #10
-	str	r3, [fp, #-52]
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	__aeabi_d2iz
-	mov	r3, r0
-	str	r3, [fp, #-48]
-	ldr	r3, .L10+4
-	sub	r4, fp, #100
-	mov	ip, r3
-	ldmia	ip!, {r0, r1, r2, r3}
+	@ args = 0, pretend = 0, frame = 48
+	@ frame_needed = 0, uses_anonymous_args = 0
+	stmfd	sp!, {r4, r5, r6, lr}
+	ldr	ip, .L30
+	mov	lr, ip
+	sub	sp, sp, #48
+	ldmia	lr!, {r0, r1, r2, r3}
+	add	r4, sp, #4
 	stmia	r4!, {r0, r1, r2, r3}
-	ldmia	ip!, {r0, r1, r2, r3}
+	ldmia	lr!, {r0, r1, r2, r3}
 	stmia	r4!, {r0, r1, r2, r3}
-	ldmia	ip, {r0, r1}
+	mov	r3, #45312
+	mov	r2, #26368
+	ldmia	lr, {r0, r1}
+	add	r3, r3, #114
+	add	r2, r2, #204
+	str	r3, [sp, #4]
+	str	r2, [sp, #8]
+	mov	r3, #2016
+	mov	r2, #1016
+	mov	ip, #14592
+	mov	lr, #7680
+	mov	r5, #3968
+	mov	r6, #508
+	add	ip, ip, #31
+	add	lr, lr, #39
 	stmia	r4, {r0, r1}
-	ldr	r0, .L10+8
+	str	r3, [sp, #24]
+	str	r2, [sp, #28]
+	mov	r3, #255
+	mov	r2, #127
+	add	r5, r5, #5
+	add	r6, r6, #2
+	ldr	r0, .L30+4
+	str	ip, [sp, #12]
+	str	lr, [sp, #16]
+	str	r3, [sp, #36]
+	str	r2, [sp, #40]
+	str	r5, [sp, #20]
+	str	r6, [sp, #32]
 	bl	printf
-	sub	r3, fp, #60
-	ldr	r0, .L10+12
-	mov	r1, r3
+	add	r1, sp, #44
+	ldr	r0, .L30+8
 	bl	scanf
-	ldr	r3, [fp, #-60]	@ float
-	mov	r0, r3
+	ldr	r0, [sp, #44]	@ float
 	bl	__aeabi_f2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
+	mov	r3, #1073741824
+	mov	r2, #0
+	add	r3, r3, #15728640
 	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
 	bl	__aeabi_d2iz
-	mov	r3, r0
-	str	r3, [fp, #-36]
-	mov	r3, #0
-	str	r3, [fp, #-56]
-	b	.L2
-.L3:
-	ldr	r7, [fp, #-56]
-	ldr	r3, [fp, #-56]
-	rsb	r3, r3, #0
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, #0
-	mov	r1, #1073741824
-	mov	r2, r3
-	mov	r3, r4
-	bl	pow
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, #0
-	mov	r6, #1069547520
-	add	r6, r6, #3145728
-	mov	r0, r3
-	mov	r1, r4
-	mov	r2, r5
-	mov	r3, r6
-	bl	__aeabi_dadd
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	log
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	__aeabi_d2iz
-	mov	r1, r0
-	mvn	r2, #71
-	mov	r3, r7, asl #2
-	sub	r0, fp, #28
-	add	r3, r0, r3
-	add	r3, r3, r2
-	str	r1, [r3, #0]
-	ldr	r3, [fp, #-56]
-	add	r3, r3, #1
-	str	r3, [fp, #-56]
-.L2:
-	ldr	r3, [fp, #-52]
-	sub	r2, r3, #1
-	ldr	r3, [fp, #-56]
-	cmp	r2, r3
-	bge	.L3
-	mov	r3, #0
-	str	r3, [fp, #-56]
-	b	.L4
-.L7:
-	ldr	r3, [fp, #-56]
-	rsb	r3, r3, #0
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, #0
-	mov	r1, #1073741824
-	mov	r2, r3
-	mov	r3, r4
-	bl	pow
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, #0
-	mov	r6, #1069547520
-	add	r6, r6, #3145728
-	mov	r0, r3
-	mov	r1, r4
-	mov	r2, r5
-	mov	r3, r6
-	bl	__aeabi_dadd
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, r3
-	mov	r6, r4
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	__aeabi_d2iz
-	mov	r3, r0
-	str	r3, [fp, #-32]
-	ldr	r3, [fp, #-56]
-	mvn	r2, #71
-	mov	r3, r3, asl #2
-	sub	r1, fp, #28
-	add	r3, r1, r3
-	add	r3, r3, r2
-	ldr	r2, [r3, #0]
-	ldr	r3, [fp, #-36]
-	rsb	r3, r2, r3
-	str	r3, [fp, #-44]
-	ldr	r3, [fp, #-48]
-	mov	r2, r3, asr #4
-	ldr	r3, [fp, #-32]
-	mov	r3, r3, asr #4
-	mul	r3, r2, r3
-	mov	r3, r3, asr #8
-	str	r3, [fp, #-40]
-	ldr	r0, [fp, #-44]
-	bl	__aeabi_i2d
-	mov	r7, r0
-	mov	r8, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, #0
-	mov	r6, #0
-	mov	r0, r3
-	mov	r1, r4
-	mov	r2, r5
-	mov	r3, r6
-	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r7
-	mov	r1, r8
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_dcmpge
-	mov	r3, r0
+	ldr	r3, [sp, #4]
+	rsb	r3, r3, r0
 	cmp	r3, #0
-	beq	.L5
-.L9:
-	ldr	r3, [fp, #-44]
-	str	r3, [fp, #-36]
-	ldr	r3, [fp, #-40]
-	str	r3, [fp, #-48]
-.L5:
-	ldr	r3, [fp, #-56]
-	add	r3, r3, #1
-	str	r3, [fp, #-56]
-.L4:
-	ldr	r3, [fp, #-52]
-	sub	r2, r3, #1
-	ldr	r3, [fp, #-56]
-	cmp	r2, r3
-	bge	.L7
-	ldr	r0, [fp, #-48]
+	ldr	r2, [sp, #8]
+	movge	r0, r3
+	movge	r1, #131072
+	movlt	r1, #65536
+	subs	ip, r0, r2
+	movpl	r2, r1, asr #4
+	movpl	r3, r2, asl #13
+	subpl	r3, r3, r2, asl #11
+	movpl	r1, r3, asr #8
+	ldr	r3, [sp, #12]
+	movpl	r0, ip
+	subs	ip, r0, r3
+	bmi	.L21
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #10
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L21:
+	ldr	r3, [sp, #16]
+	subs	ip, r0, r3
+	bmi	.L22
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #9
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L22:
+	ldr	r3, [sp, #20]
+	subs	ip, r0, r3
+	bmi	.L23
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #8
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L23:
+	ldr	r3, [sp, #24]
+	subs	ip, r0, r3
+	bmi	.L24
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #7
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L24:
+	ldr	r3, [sp, #28]
+	subs	ip, r0, r3
+	bmi	.L25
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #6
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L25:
+	ldr	r3, [sp, #32]
+	subs	ip, r0, r3
+	bmi	.L26
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #5
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L26:
+	ldr	r3, [sp, #36]
+	subs	ip, r0, r3
+	bmi	.L27
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #4
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	mov	r0, ip
+.L27:
+	cmp	r0, #126
+	movgt	r3, r1, asr #4
+	movgt	r2, r3, asl #12
+	movgt	r3, r3, asl #3
+	addgt	r3, r3, r2
+	movgt	r1, r3, asr #8
+	mov	r0, r1
 	bl	__aeabi_i2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_ddiv
-	mov	r3, r0
-	mov	r4, r1
-	ldr	r0, .L10+16
-	mov	r2, r3
-	mov	r3, r4
+	mov	r3, #1052770304
+	mov	r2, #0
+	add	r3, r3, #3145728
+	bl	__aeabi_dmul
+	mov	r2, r0
+	mov	r3, r1
+	ldr	r0, .L30+12
 	bl	printf
-	ldr	r0, [fp, #-48]
-	bl	__aeabi_i2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_ddiv
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	sub	sp, fp, #24
-	ldmfd	sp!, {r4, r5, r6, r7, r8, fp, lr}
+	mov	r0, #0
+	add	sp, sp, #48
+	ldmfd	sp!, {r4, r5, r6, lr}
 	bx	lr
-.L11:
+.L31:
 	.align	2
-.L10:
-	.word	scale
-	.word	C.1.2188
+.L30:
+	.word	.LANCHOR0
 	.word	.LC0
 	.word	.LC1
 	.word	.LC2
 	.size	main, .-main
+	.global	scale
+	.global	K
+	.section	.rodata
+	.align	2
+.LANCHOR0 = . + 0
+	.type	C.14.2324, %object
+	.size	C.14.2324, 40
+C.14.2324:
+	.space	40
+	.type	scale, %object
+	.size	scale, 4
+scale:
+	.word	16
+	.type	K, %object
+	.size	K, 4
+K:
+	.word	10
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"Please enter a number between 0 and 1: \000"
+.LC1:
+	.ascii	"%f\000"
+	.space	1
+.LC2:
+	.ascii	"\012The Exponential Value is = %.4f\012\000"
 	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
 	.section	.note.GNU-stack,"",%progbits
--- log.s
+++ log.s
@@ -6,353 +6,314 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 2
 	.eabi_attribute 18, 4
 	.file	"log.c"
-	.global	scale
-	.section	.rodata
-	.align	2
-	.type	scale, %object
-	.size	scale, 4
-scale:
-	.word	16
-	.align	2
-	.type	C.1.2191, %object
-	.size	C.1.2191, 40
-C.1.2191:
-	.space	40
-	.global	__aeabi_i2d
+	.global	__aeabi_f2d
 	.global	__aeabi_dmul
 	.global	__aeabi_d2iz
-	.align	2
-.LC0:
-	.ascii	"Please enter a number between .5 and 1: \000"
-	.align	2
-.LC1:
-	.ascii	"%f\000"
-	.global	__aeabi_f2d
-	.global	__aeabi_dadd
-	.align	2
-.LC2:
-	.ascii	" table location %d, value:  %d \012\000"
-	.global	__aeabi_dcmple
-	.global	__aeabi_ddiv
-	.align	2
-.LC3:
-	.ascii	"\012The log Value is = %.4f\012\000"
+	.global	__aeabi_i2d
 	.text
+	.align	2
+	.global	Log
+	.type	Log, %function
+Log:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 0, uses_anonymous_args = 0
+	stmfd	sp!, {r4, lr}
+	mov	r4, r0
+	sub	sp, sp, #8
+	ldr	r0, .L14
+	bl	printf
+	add	r1, sp, #4
+	ldr	r0, .L14+4
+	bl	scanf
+	ldr	r0, [sp, #4]	@ float
+	bl	__aeabi_f2d
+	mov	r3, #1073741824
+	mov	r2, #0
+	add	r3, r3, #15728640
+	bl	__aeabi_dmul
+	bl	__aeabi_d2iz
+	mov	r0, r0, asr #4
+	mov	r2, r0, asl #13
+	cmp	r2, #16777216
+	ldr	r3, [r4, #0]
+	movle	r0, r2, asr #12
+	rsble	ip, r3, #0
+	mov	r3, r0, asl #13
+	sub	r3, r3, r0, asl #11
+	movgt	ip, #0
+	ldr	r2, [r4, #4]
+	cmp	r3, #16777216
+	movle	r0, r3, asr #12
+	rsble	ip, r2, ip
+	mov	r3, r0, asl #12
+	mov	r2, r0, asl #10
+	add	r2, r2, r3
+	cmp	r2, #16777216
+	ldr	r3, [r4, #8]
+	movle	r0, r2, asr #12
+	rsble	ip, r3, ip
+	mov	r2, r0, asl #9
+	mov	r3, r0, asl #12
+	add	r2, r2, r3
+	cmp	r2, #16777216
+	ldr	r3, [r4, #12]
+	movle	r0, r2, asr #12
+	rsble	ip, r3, ip
+	mov	r2, r0, asl #8
+	mov	r3, r0, asl #12
+	add	r2, r2, r3
+	cmp	r2, #16777216
+	ldr	r3, [r4, #16]
+	movle	r0, r2, asr #12
+	rsble	ip, r3, ip
+	mov	r2, r0, asl #12
+	mov	r3, r0, asl #7
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	cmp	r1, #65536
+	ldr	r2, [r4, #20]
+	movle	r0, r3, asr #12
+	rsble	ip, r2, ip
+	mov	r3, r0, asl #6
+	mov	r2, r0, asl #12
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	cmp	r1, #65536
+	ldr	r2, [r4, #24]
+	movle	r0, r3, asr #12
+	rsble	ip, r2, ip
+	mov	r3, r0, asl #5
+	mov	r2, r0, asl #12
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	cmp	r1, #65536
+	ldr	r2, [r4, #28]
+	movle	r0, r3, asr #12
+	rsble	ip, r2, ip
+	mov	r3, r0, asl #4
+	mov	r2, r0, asl #12
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	cmp	r1, #65536
+	ldr	r2, [r4, #32]
+	movle	r0, r3, asr #12
+	rsble	ip, r2, ip
+	mov	r3, r0, asl #3
+	mov	r2, r0, asl #12
+	add	r3, r3, r2
+	mov	r3, r3, asr #8
+	ldr	r0, [r4, #36]
+	cmp	r3, #65536
+	rsble	ip, r0, ip
+	mov	r0, ip
+	bl	__aeabi_i2d
+	mov	r3, #1052770304
+	mov	r2, #0
+	add	r3, r3, #3145728
+	bl	__aeabi_dmul
+	add	sp, sp, #8
+	ldmfd	sp!, {r4, lr}
+	bx	lr
+.L15:
+	.align	2
+.L14:
+	.word	.LC0
+	.word	.LC1
+	.size	Log, .-Log
 	.align	2
 	.global	main
 	.type	main, %function
 main:
 	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 80
-	@ frame_needed = 1, uses_anonymous_args = 0
-	stmfd	sp!, {r4, r5, r6, r7, fp, lr}
-	add	fp, sp, #20
-	sub	sp, sp, #80
-	mov	r3, #10
-	str	r3, [fp, #-48]
-	mov	r3, #0
-	str	r3, [fp, #-44]
-	ldr	r0, [fp, #-44]
-	bl	__aeabi_i2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	__aeabi_d2iz
-	mov	r3, r0
-	str	r3, [fp, #-32]
-	ldr	r3, .L10+4
-	sub	r4, fp, #96
-	mov	ip, r3
-	ldmia	ip!, {r0, r1, r2, r3}
+	@ args = 0, pretend = 0, frame = 48
+	@ frame_needed = 0, uses_anonymous_args = 0
+	stmfd	sp!, {r4, r5, r6, lr}
+	ldr	ip, .L37
+	mov	lr, ip
+	sub	sp, sp, #48
+	ldmia	lr!, {r0, r1, r2, r3}
+	add	r4, sp, #4
 	stmia	r4!, {r0, r1, r2, r3}
-	ldmia	ip!, {r0, r1, r2, r3}
+	ldmia	lr!, {r0, r1, r2, r3}
 	stmia	r4!, {r0, r1, r2, r3}
-	ldmia	ip, {r0, r1}
+	mov	r3, #45312
+	mov	r2, #26368
+	ldmia	lr, {r0, r1}
+	add	r3, r3, #114
+	add	r2, r2, #204
+	str	r3, [sp, #4]
+	str	r2, [sp, #8]
+	mov	r3, #2016
+	mov	r2, #1016
+	mov	ip, #14592
+	mov	lr, #7680
+	mov	r5, #3968
+	mov	r6, #508
+	add	lr, lr, #39
+	add	ip, ip, #31
 	stmia	r4, {r0, r1}
-	ldr	r0, .L10+8
+	str	r3, [sp, #24]
+	str	r2, [sp, #28]
+	mov	r3, #255
+	mov	r2, #127
+	add	r5, r5, #5
+	add	r6, r6, #2
+	ldr	r0, .L37+4
+	str	lr, [sp, #16]
+	str	ip, [sp, #12]
+	str	r3, [sp, #36]
+	str	r2, [sp, #40]
+	str	r5, [sp, #20]
+	str	r6, [sp, #32]
 	bl	printf
-	sub	r3, fp, #56
-	ldr	r0, .L10+12
-	mov	r1, r3
+	add	r1, sp, #44
+	ldr	r0, .L37+8
 	bl	scanf
-	ldr	r3, [fp, #-56]	@ float
-	mov	r0, r3
+	ldr	r0, [sp, #44]	@ float
 	bl	__aeabi_f2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
+	mov	r3, #1073741824
+	mov	r2, #0
+	add	r3, r3, #15728640
 	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
 	bl	__aeabi_d2iz
-	mov	r3, r0
-	str	r3, [fp, #-28]
-	mov	r3, #0
-	str	r3, [fp, #-52]
-	b	.L2
-.L3:
-	ldr	r7, [fp, #-52]
-	ldr	r3, [fp, #-52]
-	rsb	r3, r3, #0
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, #0
-	mov	r1, #1073741824
-	mov	r2, r3
-	mov	r3, r4
-	bl	pow
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, #0
-	mov	r6, #1069547520
-	add	r6, r6, #3145728
-	mov	r0, r3
-	mov	r1, r4
-	mov	r2, r5
-	mov	r3, r6
-	bl	__aeabi_dadd
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	log
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	__aeabi_d2iz
-	mov	r1, r0
-	mvn	r2, #75
-	mov	r3, r7, asl #2
-	sub	r0, fp, #20
-	add	r3, r0, r3
+	mov	r3, r0, asr #4
+	mov	r3, r3, asl #13
+	mov	ip, r3, asr #8
+	cmp	ip, #65536
+	mov	r2, r0
+	movgt	ip, r2
+	mov	r2, ip, asr #4
+	mov	r3, r2, asl #13
+	ldr	r0, [sp, #4]
+	sub	r3, r3, r2, asl #11
+	mov	r1, r3, asr #8
+	movgt	r0, #0
+	rsble	r0, r0, #0
+	ldr	r3, [sp, #8]
+	cmp	r1, #65536
+	movgt	r1, ip
+	rsble	r0, r3, r0
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #10
 	add	r3, r3, r2
-	str	r1, [r3, #0]
-	ldr	r3, [fp, #-52]
-	mvn	r2, #75
-	mov	r3, r3, asl #2
-	sub	r1, fp, #20
-	add	r3, r1, r3
+	mov	ip, r3, asr #8
+	cmp	ip, #65536
+	ldr	r3, [sp, #12]
+	movgt	ip, r1
+	rsble	r0, r3, r0
+	mov	r3, ip, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #9
 	add	r3, r3, r2
-	ldr	r3, [r3, #0]
-	ldr	r0, .L10+16
-	ldr	r1, [fp, #-52]
-	mov	r2, r3
-	bl	printf
-	ldr	r3, [fp, #-52]
-	add	r3, r3, #1
-	str	r3, [fp, #-52]
-.L2:
-	ldr	r3, [fp, #-48]
-	sub	r2, r3, #1
-	ldr	r3, [fp, #-52]
-	cmp	r2, r3
-	bge	.L3
-	mov	r3, #0
-	str	r3, [fp, #-52]
-	b	.L4
-.L7:
-	ldr	r3, [fp, #-52]
-	rsb	r3, r3, #0
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, #0
-	mov	r1, #1073741824
-	mov	r2, r3
-	mov	r3, r4
-	bl	pow
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, #0
-	mov	r6, #1069547520
-	add	r6, r6, #3145728
-	mov	r0, r3
-	mov	r1, r4
-	mov	r2, r5
-	mov	r3, r6
-	bl	__aeabi_dadd
-	mov	r3, r0
-	mov	r4, r1
-	mov	r5, r3
-	mov	r6, r4
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_dmul
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	bl	__aeabi_d2iz
-	mov	r3, r0
-	str	r3, [fp, #-24]
-	ldr	r3, [fp, #-28]
-	mov	r2, r3, asr #4
-	ldr	r3, [fp, #-24]
-	mov	r3, r3, asr #4
-	mul	r3, r2, r3
+	mov	r1, r3, asr #8
+	cmp	r1, #65536
+	ldr	r3, [sp, #16]
+	movgt	r1, ip
+	rsble	r0, r3, r0
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #8
+	add	r3, r3, r2
+	mov	ip, r3, asr #8
+	cmp	ip, #65536
+	ldr	r3, [sp, #20]
+	movgt	ip, r1
+	rsble	r0, r3, r0
+	mov	r3, ip, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #7
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	cmp	r1, #65536
+	ldr	r3, [sp, #24]
+	movgt	r1, ip
+	rsble	r0, r3, r0
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #6
+	add	r3, r3, r2
+	mov	ip, r3, asr #8
+	cmp	ip, #65536
+	ldr	r3, [sp, #28]
+	movgt	ip, r1
+	rsble	r0, r3, r0
+	mov	r3, ip, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #5
+	add	r3, r3, r2
+	mov	r1, r3, asr #8
+	cmp	r1, #65536
+	ldr	r3, [sp, #32]
+	movgt	r1, ip
+	rsble	r0, r3, r0
+	mov	r3, r1, asr #4
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #4
+	add	r3, r3, r2
 	mov	r3, r3, asr #8
-	str	r3, [fp, #-40]
-	ldr	r3, [fp, #-52]
-	mvn	r2, #75
-	mov	r3, r3, asl #2
-	sub	r0, fp, #20
-	add	r3, r0, r3
+	cmp	r3, #65536
+	ldr	r2, [sp, #36]
+	movgt	r3, r1
+	mov	r3, r3, asr #4
+	rsble	r0, r2, r0
+	mov	r2, r3, asl #12
+	mov	r3, r3, asl #3
 	add	r3, r3, r2
-	ldr	r3, [r3, #0]
-	rsb	r3, r3, #0
-	str	r3, [fp, #-36]
-	ldr	r0, [fp, #-40]
+	mov	r3, r3, asr #8
+	cmp	r3, #65536
+	suble	r0, r0, #127
 	bl	__aeabi_i2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_dcmple
-	mov	r3, r0
-	cmp	r3, #0
-	beq	.L5
-.L9:
-	ldr	r3, [fp, #-40]
-	str	r3, [fp, #-28]
-	ldr	r3, [fp, #-36]
-	str	r3, [fp, #-32]
-.L5:
-	ldr	r3, [fp, #-52]
-	add	r3, r3, #1
-	str	r3, [fp, #-52]
-.L4:
-	ldr	r3, [fp, #-48]
-	sub	r2, r3, #1
-	ldr	r3, [fp, #-52]
-	cmp	r2, r3
-	bge	.L7
-	ldr	r0, [fp, #-32]
-	bl	__aeabi_i2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_ddiv
-	mov	r3, r0
-	mov	r4, r1
-	ldr	r0, .L10+20
-	mov	r2, r3
-	mov	r3, r4
+	mov	r3, #1052770304
+	mov	r2, #0
+	add	r3, r3, #3145728
+	bl	__aeabi_dmul
+	mov	r2, r0
+	mov	r3, r1
+	ldr	r0, .L37+12
 	bl	printf
-	ldr	r0, [fp, #-32]
-	bl	__aeabi_i2d
-	mov	r5, r0
-	mov	r6, r1
-	ldr	r3, .L10
-	ldr	r2, [r3, #0]
-	mov	r3, #1
-	mov	r3, r3, asl r2
-	mov	r0, r3
-	bl	__aeabi_i2d
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r5
-	mov	r1, r6
-	mov	r2, r3
-	mov	r3, r4
-	bl	__aeabi_ddiv
-	mov	r3, r0
-	mov	r4, r1
-	mov	r0, r3
-	mov	r1, r4
-	sub	sp, fp, #20
-	ldmfd	sp!, {r4, r5, r6, r7, fp, lr}
+	mov	r0, #0
+	add	sp, sp, #48
+	ldmfd	sp!, {r4, r5, r6, lr}
 	bx	lr
-.L11:
+.L38:
 	.align	2
-.L10:
-	.word	scale
-	.word	C.1.2191
+.L37:
+	.word	.LANCHOR0
 	.word	.LC0
 	.word	.LC1
 	.word	.LC2
-	.word	.LC3
 	.size	main, .-main
+	.global	scale
+	.global	K
+	.section	.rodata
+	.align	2
+.LANCHOR0 = . + 0
+	.type	C.14.2327, %object
+	.size	C.14.2327, 40
+C.14.2327:
+	.space	40
+	.type	scale, %object
+	.size	scale, 4
+scale:
+	.word	16
+	.type	K, %object
+	.size	K, 4
+K:
+	.word	10
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"Please enter a number between .5 and 1: \000"
+	.space	3
+.LC1:
+	.ascii	"%f\000"
+	.space	1
+.LC2:
+	.ascii	"\012The log Value is = %.4f\012\000"
 	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
 	.section	.note.GNU-stack,"",%progbits
