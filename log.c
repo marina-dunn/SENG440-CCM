6b06f3f41758731e09708a40e49e392420c8ee90
DrydenLinden <Drydenlinden@hotmail.com>
2020-08-14 17:47:08 -0700

    Update log.c

--- log.c
+++ log.c
@@ -13,7 +13,6 @@ const int scale = 16;
 #define DIV(x,y) (((x)<<8)/(y)<<8)
 const int K = 10; //precision
 
-
 double Log(int table[10]){
     int i;
     float M;
@@ -22,42 +21,23 @@ double Log(int table[10]){
     int m_fixed = DoubleToFixed(M);
     int x = DoubleToFixed(1);    
     int r = 0, add, shift;
-    int res = DoubleToFixed(r);
-
-    
+    int res = DoubleToFixed(r);  
     for(i=0;i<=(K-1);i++)
     {
         int temp = DoubleToFixed((1.0+pow(2, -i)));
         add = MUL(m_fixed, temp);
         shift = res - table[i];
-        /*
-            if(add <= DoubleToFixed(1)){
-            m_fixed = add;
-            res = shift;
-        */
         add <= x ? m_fixed = add, res = shift : 0;
     }
     return FixedToDouble(res);
 }
-
 int main()
 {
-
-
     int i;
     int table[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-
     for(i=0; i <= (K-1); i++){
          table[i] = DoubleToFixed(log(1+pow(2, -i)));
     }
-    
-        /*
-        asm ("cmp %2, %3 ; movle %0, %2; movle %1, %4;" 
-             : "=r"(m_fixed), "=r"(res) 
-             : "r"(add), "r"(x) , "r"(shift));  
-       */   
-    
 
     printf("\nThe log Value is = %.4f\n", Log(table));
     return 0;
